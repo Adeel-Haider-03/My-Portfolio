90e963a17d57e0789a5df137c24ac48c590cba5a
Adeel Haider <146270709+Adeel-Haider-03@users.noreply.github.com>
2024-09-06 20:36:11 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
 
 <footer id='footer'>
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-    <span className="text-sm text-green-500 sm:text-center">© 2024 Portfolio . All Rights Reserved.</span>
-    <span className='text-green-600 sm:text-center'>Made with <span className='text-red-700' style={{ fontSize: '2.5rem' }}>&#9825;</span> by Adeel</span>
+    <h5 className="text-green-600 sm:text-center">© 2024 Portfolio . All Rights Reserved.</h5>
+    <h5 className='text-green-600 sm:text-center'>Made with <span className='text-red-700' style={{ fontSize: '2.5rem' }}>&#9825;</span> by Adeel</h5>
     </div>
 </footer>
 
